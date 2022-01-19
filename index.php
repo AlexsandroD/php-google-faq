f4806021ee742d2cfd90957d1402121285df6721
AlexsandroD <adrdra88@gmail.com>
2022-01-19 17:22:29 +0100

    end

--- index.php
+++ index.php
@@ -3,47 +3,52 @@
         $qa = [
             'question' => "How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?",
 
-            'answer' => "The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.
+            'answer' => "<span?>The recent <a>ruling by the Court of Justice of the European Union</a> has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.</span>
 
-            Since this ruling was published on 13 May 2014, we've been working around the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with the public's right to know and distribute information.
+            <span>Since this ruling was published on 13 May 2014, we've been working around the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with the public's right to know and distribute information.</span>
 
-            If you have a removal request, please fill out this web form. You'll receive an automatic reply confirming that we have received your request. We will then assess your case – please note that this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results – for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organisation, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.
+            <span>If you have a removal request, please fill out this <a>web form</a>. You'll receive an automatic reply confirming that we have received your request. We will then assess your case – please note that this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results – for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organisation, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.</span>
 
-            We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe that it's important to respect the Court's judgement and we are working hard to devise a process that complies with the law.
+            <span>We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe that it's important to respect the Court's judgement and we are working hard to devise a process that complies with the law.</span>
 
-            When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.",
+            <span>When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.</span>",
         ],
         $qa2 =[
             'question' => "How does Google protect my privacy and keep my information secure?",
 
-            'answer' => "We know that security and privacy are important to you - and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.
-            We’re constantly working to ensure strong security, protect your privacy and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We've also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.
+            'answer' => "<span>We know that security and privacy are important to you - and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.</span>
 
-            You can find out more about safety and security online, including how to protect yourself and your family online, at the Google Safety Centre.
+            <span>We’re constantly working to ensure strong security, protect your privacy and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We've also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.</span>
 
-            Find out more about how we keep your personal information private and safe – and put you in control.",
+            <span>You can find out more about safety and security online, including how to protect yourself and your family online, at the <a>Google Safety Centre</a>.</span>
+
+            <span>Find out more about how we keep your personal information private and safe – and put you in control.</span>",
                     ],
         $qa3 =[
             'question' => "Why is my account associated with a country?",
 
-            'answer' => "Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:
+            'answer' => "<span>Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:</span>
             <ol>
             <li>The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. Generally, Google offers its consumer services through either of two companies:
-                
+
             <ol type='a'>    
             <li>Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland</li>
             <li>Google LLC, based in the United States, for the rest of the world</li></ol></li>
 
             <li>The version of the terms that govern our relationship, which can vary depending on local laws</li>
             </ol>
-            Keep in mind that Google services are essentially the same, regardless of the affiliate that provides the services or your country association.
+            <span>Keep in mind that Google services are essentially the same, regardless of the affiliate that provides the services or your country association.</span>
 
-            Determining the country associated with your account
-            When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services – typically where you’ve spent the most time in the last year.
+            <h2>Determining the country associated with your account</h2>
+            <span>
 
-            Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.
+            <span>When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services – typically where you’ve spent the most time in the last year.</span>
 
-            If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. Contact us if you think that your country association is wrong.",
+            <span>Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.</span>
+
+            <span>If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. Contact us if you think that your country association is wrong.</span>
+
+        ",
         ],
         $qa4 =[
             'question' => "How can I remove information about myself from Google’s search results?",
@@ -68,12 +73,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Google faq</title>
 </head>
+<style>
+<?php include 'style/style.css'; ?>
+</style>
 <body>
-    <?php foreach ($faqs as $key => $faq ) { ?>
-        <h1><?php echo $faq["question"] ?></h1>
-        <p><?php echo $faq["answer"] ?></p>
-        <?php } ?>
+        <header>
+            <div class="logo"><img src="google.svg" alt="google logo"></div>
+            <div class="text"><div>Privacy & Terms</div></div>
+        </header>
+        <div class="container"> 
+            <?php foreach ($faqs as $key => $faq ) { ?>
+            <div class="faq-container">
+                    <h1 class="question"><?php echo $faq["question"] ?></h1>
+                    <p class="answer"><?php echo $faq["answer"] ?></p>
+                <?php } ?> 
+            </div>
+        </div>
+
 </body>
 </html>
